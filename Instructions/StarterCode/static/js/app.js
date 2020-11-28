e24460ca3fc7e4b0309598e1336e4fffcda272e6
Trent Litte <trent.e.little@gmail.com>
2020-11-28 12:44:04 -0500

    creation of button element in app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -5,3 +5,25 @@ console.log(tableData)
 // table body reference
 var tbody = d3.select("tbody");
 
+
+//call to the values for UFO sightings
+tableData.forEach(function(sighting) {
+	console.log(sighting);
+	var row = tbody.append("tr");
+
+	//log each ufo sighting value
+	Object.entries(sighting).forEach(function([key, value]) {
+		console.log(key, value);
+		var cell = row.append("td")
+		cell.text(value);
+	});
+});
+
+
+//click on the button 
+var button = d3.select("#filter-btn");
+button.on("click", funtion() {
+	tbody.html("");
+
+	//select input date and get the date, state, and shape of value
+})
