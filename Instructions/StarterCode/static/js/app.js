376c023dd77a5ef1cbe6820085cf5fb381516230
Trent Litte <trent.e.little@gmail.com>
2020-11-28 13:07:19 -0500

    updated app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,6 +1,6 @@
 // from data.js
 var tableData = data;
-console.log(tableData)
+console.log(tableData);
 
 // table body reference
 var tbody = d3.select("tbody");
@@ -14,16 +14,35 @@ tableData.forEach(function(sighting) {
 	//log each ufo sighting value
 	Object.entries(sighting).forEach(function([key, value]) {
 		console.log(key, value);
-		var cell = row.append("td")
+		var cell = row.append("td");
 		cell.text(value);
 	});
 });
-
 
 //click on the button 
 var button = d3.select("#filter-btn");
 button.on("click", funtion() {
 	tbody.html("");
 
-	//select input date and get the date, state, and shape of value
-})
+	//select input date and get the date, state, and shape of returned values
+	var inputDate = d3.select("#datetime");
+	var inputReturn = inputDate.property("value");
+	console.log(inputReturn);
+
+	//Filter datetime to equal the inputReturn
+	var fileteredData = tableData.filter(sighting => sighting.datetime === inputReturn);
+	console.log(fileteredData);
+
+	fileteredData.forEach(function(results) {
+		console.log(results);
+
+		//append  the results one table row and cell for each result
+		var tr = tbody.append("tr");
+		Object.entries(results).forEach(function([key, value]) {
+			console.log(key, value);
+			var td = row.append("td");
+			td.text(value)
+		});
+
+	});
+});
