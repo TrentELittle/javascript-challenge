59758f2b4473be08f27c8e593cd37ff39048c594
Trent Litte <trent.e.little@gmail.com>
2020-11-28 16:19:54 -0500

    final app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,14 +1,14 @@
-// from data.js
 var tableData = data;
 
+//utilize d3 to select the tbody
 let tbody = d3.select("tbody");
 
-function buildTable(data){
-
+//Create function that builds a table with the data
+function table(data){
     tbody.html("");
 
+	//append the rows(tr) and cells(td) for returned values
     data.forEach((dataRow) => {
-
         let row = tbody.append("tr");
                
         Object.values(dataRow).forEach((val) => {
@@ -18,18 +18,24 @@ function buildTable(data){
     })
 }
 
-function handleClick(){
+//Create button function for filtering through returned data by datetime that is entered
+function button(){
     d3.event.preventDefault();
 
-    let date = d3.select("#datetime").property("value");
+    //create definitions for data value and the filtered data
+    let dateValue = d3.select("#datetime").property("value");
     let filterData = tableData;
 
-    if(date) {
-        filterData = filterData.filter((row) => row.datetime === date);
+    //Create if statement to filter through the data by the entered date value
+    if(dateValue) {
+        filterData = filterData.filter((row) => row.datetime === dateValue);
     }
-    buildTable(filterData);
+
+    //Create table with filtered Data
+	table(filterData);
 }
 
-d3.selectAll("#filter-btn").on('click', handleClick);
 
-buildTable(tableData);
+//Use d3 to call to the button click and present table
+d3.selectAll("#filter-btn").on('click', button);
+table(tableData);
